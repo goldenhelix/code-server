2e0c2ceee352e76c4ad4c72417c0c8c179b4c349
Gabe Rudy <rudy@goldenhelix.com>
2025-03-11 08:18:54 -0600

    Update to 4.98.0-rc.1 and fix patches

--- build.sh
+++ build.sh
@@ -2,6 +2,35 @@
 
 # Build a Golden Helix docker image for code-server with changes to support App Streaming on VSWarehouse
 
+# Form maintaing the code-server image, add the upstream repository if you haven't already
+# git remote add upstream https://github.com/coder/code-server.git
+
+# Fetch the latest changes from upstream
+# git fetch upstream
+
+# Checkout your main branch
+# git checkout main
+
+# Pop all quilt patches
+# quilt pop -a
+
+# If thee are still changes to the vscode lib, reset them
+# git -C lib/vscode checkout -- .
+
+# Merge the changes from upstream/main to your local main branch
+# git merge upstream/main
+
+# Apply the patches
+# quilt push -a
+
+# If a patch fails, you need to merge fix it, for example if shutdown.diff fails
+# quilt push -f shutdown.diff # Needed to apply as much of the changes as possible
+# Manually fix failed chunks/files
+# quilt refresh
+# quilt push -a (continue until all patches are applied)
+
+# Handle any merge conflicts if they arise
+
 # Follow the directions under [CONTRIBUTING.md](docs/CONTRIBUTING.md) to build the image
 # See ./package.sh for these commands
 
@@ -21,12 +50,15 @@
 # Run install_system_extensions.sh to extensions listed in extensions.txt
 # ./install_system_extensions.sh
 
+# Note that lib/vscode/package.json may need the following change (remove --max-old-space-size=8192 )
+#    "gulp": "node ./node_modules/gulp/bin/gulp.js",
+
 # Add "code" as symlink
 cd release-standalone/lib/vscode/bin/remote-cli/
 ln -f -s code-linux.sh code
 cd ../../../../../
 
-export VERSION=4.96.2
+export VERSION=4.98.0-rc.1
 
 # Ensure we're in the correct directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -48,4 +80,4 @@ if [ -f "./node" ]; then
 fi
 
 # Run like
-# docker run -it  -p 8081:8080 -e PASSWORD=your_secure_password123 -e PORT=8080 -e IDLE_TIMEOUT=2  registry.goldenhelix.com/gh/code-server:4.96.2 /home/ghuser/Workspace/
+# docker run -it  -p 8081:8080 -e PASSWORD=your_secure_password123 -e PORT=8080 -e IDLE_TIMEOUT=2  registry.goldenhelix.com/public/code-server:4.98.0-rc.1 /home/ghuser/Workspace/
--- package.sh
+++ package.sh
@@ -1,8 +1,9 @@
 npm run build
-# VERSION=4.96.2 npm run build:vscode
+#export NODE_OPTIONS="--max-old-space-size=16384" 
+VERSION=4.98.0-rc.1 npm run build:vscode
 npm run release
 
-VERSION=4.96.2 npm run package
+VERSION=4.98.0-rc.1 npm run package
 cd release
 npm install --omit=dev
 cd ..
